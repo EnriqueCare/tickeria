2cad9a9636a739c614cba03e4a2d7ba29b49330b
EnriqueCare <jeec_m13@outlook.es>
2026-05-21 05:06:54 -0600

    Estructura inicial completa: Base de datos en Docker, Backend en Spring Boot y Frontend en Vue.js

---
+++ tickeria-backend/src/main/java/tickeria_backend/TickeriaBackendApplication.java
@@ -0,0 +1,13 @@
+package tickeria_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickeriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickeriaBackendApplication.class, args);
+	}
+
+}
---
+++ tickeria-backend/src/test/java/tickeria_backend/TickeriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tickeria_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickeriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
